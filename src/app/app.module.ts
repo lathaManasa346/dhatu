6a80b7a77658a14f1e7825fb4dc84bd06d3d4358
lathaManasa346 <akandy056@gmail.com>
2025-04-03 12:34:35 -0500

    home page changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
-import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurtainComponent } from './curtain/curtain.component';
 import { HomeComponent } from './home/home.component';
+import { HomeContentComponent } from './home-content/home-content.component';
+import { HomeDetailsComponent } from './home-details/home-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CurtainComponent,
-    HomeComponent
+    HomeComponent,
+    HomeContentComponent,
+    HomeDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-content/home-content.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-content',
+  templateUrl: './home-content.component.html',
+  styleUrls: ['./home-content.component.scss']
+})
+export class HomeContentComponent implements OnInit {
+  slideIndex: number = 1;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.showSlides(this.slideIndex);
+  }
+
+  plusSlides(n: number): void {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n: number): void {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number): void {
+    let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    let dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+
+    if (n > slides.length) { this.slideIndex = 1; }
+    if (n < 1) { this.slideIndex = slides.length; }
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+  }
+}
